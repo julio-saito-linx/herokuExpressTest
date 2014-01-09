9e54098de4d66eeb04a161a3307b68cf6ac88068
Julio Makdisse Saito <saitodisse@gmail.com>
2014-01-09 19:17:26 -0200

    adding mongo

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 // web.js
 var express = require("express");
+var mongo = require('mongodb');
 var logfmt = require("logfmt");
 var app = express();
 
@@ -13,3 +14,15 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+//MONGO-DB
+var mongoUri =  process.env.MONGOLAB_URI  ||
+                process.env.MONGOHQ_URL   ||
+  'mongodb://admin:admin@ds061198.mongolab.com:61198/heroku_app21107224';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});  
